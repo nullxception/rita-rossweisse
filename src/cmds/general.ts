1e30658a32336627f6a9c8d9f52f18e5aa6f9d64
Nauval Rizky <enuma.alrizky@gmail.com>
2022-01-14 07:39:12 +0700

    add simpler vibe check

--- src/cmds/general.ts
+++ src/cmds/general.ts
@@ -23,3 +23,12 @@ rita.hears(/hey rita,.*(a.+|)r.*(y.+|)u ther/gim, async (ctx) => {
     reply_to_message_id: ctx.update.message.message_id,
   });
 });
+
+rita.hears(/hey rita$/gim, async (ctx) => {
+  if (ctx.update.message.from.username != username) return;
+  if (isOldMessage(ctx.update.message.date)) return;
+
+  ctx.reply(`at your service, kanchou-sama`, {
+    reply_to_message_id: ctx.update.message.message_id,
+  });
+});
